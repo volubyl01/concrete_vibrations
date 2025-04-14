82d4c333e16eea1943d60852a6e0bfa6d577a3cc
volubyl01 <myriam.braconier@gmail.com>
2025-04-14 15:02:51 +0200

    creation entity information

---
+++ migrations/Version20250414130123.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250414130123 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE information (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, country VARCHAR(2) DEFAULT NULL, first_name VARCHAR(15) NOT NULL, last_name VARCHAR(50) NOT NULL, UNIQUE INDEX UNIQ_29791883A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE information ADD CONSTRAINT FK_29791883A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE information DROP FOREIGN KEY FK_29791883A76ED395
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE information
+        SQL);
+    }
+}
