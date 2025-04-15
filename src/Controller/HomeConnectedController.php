7e01ceb65f33319610ab979dc0d2fba320a813d4
volubyl01 <myriam.braconier@gmail.com>
2025-04-15 19:01:18 +0200

    fonts-favicon et homeconnected

--- src/Controller/HomeConnectedController.php
+++ src/Controller/HomeConnectedController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class HomeConnectedController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/home/connected', name: 'app_home_connected')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('home_connected/index.html.twig', [
+            'controller_name' => 'HomeConnectedController',
         ]);
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -23,4 +23,10 @@ class SecurityController extends AbstractController
             'error' => $error,
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
 }
